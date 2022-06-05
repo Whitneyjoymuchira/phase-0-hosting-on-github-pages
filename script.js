84fc185152fe521b483ff1f43a9fb33bb5977bcd
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-05 21:39:51 +0300

    removed typing effect

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ menu.addEventListener('click', function(){
     menuLinks.classList.toggle('active');
 })
 
-
+var i=0;
+var txt ='BONJOUR!';
+var speed=50;
+function typeWriter(){
+    if (i<txt.length){
+        document.getElementsById("typed-out").innerHTML +=txt.charAt(i);
+    i++;
+    setTimeout(typeWriter,speed);
+    }
+}
