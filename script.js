4bb3f9612a1a82bd97c0a4b6b29097c07040d8b2
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-05 15:18:47 +0300

    added content container in html

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const menu=document.querySelector('mobile-menu');
-const menuLInks=document.querySelector('.nav-menu');
+const menuLinks=document.querySelector('.nav-menu');
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
-    menu.classList.toggle('active');
+    menuLinks.classList.toggle('active');
 })
 
 
